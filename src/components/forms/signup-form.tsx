738fbd2bc90461feb2d8df81f24b9b7b8e3f9c32
ammar-333 <ammarshaban5@gmail.com>
2026-02-15 16:31:00 +0300

    update the youth houses resrvation page

--- src/components/forms/signup-form.tsx
+++ src/components/forms/signup-form.tsx
@@ -11,11 +11,33 @@ import { Input } from "@/components/ui/input";
 import { LogIn } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
-import { Label } from "../ui/label";
+import { useMemo, useState } from "react";
+// import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+// import { Label } from "../ui/label";
+import { z } from "zod";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-type companySectorType = "government" | "private";
+const formSchema = z.object({
+  orgNationalId: z.number(),
+  orgName: z.string(),
+  capacity: z.enum(["owner", "Written", "register"]),
+  nationality: z.enum(["jordanian", "non-jordanian"]),
+  commissionerID: z.number(),
+  email: z.string().email().optional(),
+  phone: z.string(),
+  commissionerFile: z.instanceof(File).optional(),
+});
+type Option = { value: string; label: string };
+// type companySectorType = "government" | "private";
+type formType = z.infer<typeof formSchema>;
+type FormErrors = Partial<Record<keyof formType, string>>;
 
 export default function SignupForm({
   className,
@@ -23,18 +45,91 @@ export default function SignupForm({
 }: React.ComponentProps<"div">) {
   const { t } = useTranslation();
   const navigate = useNavigate();
-  const [companySector, setCompanySector] =
-    useState<companySectorType>("government");
-  const [email, setEmail] = useState("");
-  const [Phone, setPhone] = useState("");
+  // const [companySector, setCompanySector] =
+  //   useState<companySectorType>("government");
+  const [form, setForm] = useState<formType>({
+    orgNationalId: undefined,
+    orgName: undefined,
+    capacity: undefined,
+    nationality: undefined,
+    commissionerID: undefined,
+    email: undefined,
+    phone: undefined,
+    commissionerFile: undefined,
+  });
+  const [formErrors, setFormErrors] = useState<FormErrors>({});
 
-  useEffect(() => {
-    console.log(companySector);
-  }, [companySector]);
+  const capacityOptions: Option[] = useMemo(
+    () => [
+      {
+        value: "owner",
+        label: t("reservation.options.commissionerCapicity.owner"),
+      },
+      {
+        value: "Written",
+        label: t("reservation.options.commissionerCapicity.Written"),
+      },
+      {
+        value: "register",
+        label: t("reservation.options.commissionerCapicity.register"),
+      },
+    ],
+    [t],
+  );
 
-  const handleSubmit = () => {
-    navigate("/organization-profile");
-  };
+  const nationalityOptions: Option[] = useMemo(
+    () => [
+      {
+        value: "jordanian",
+        label: t("reservation.options.nationality.jordanian"),
+      },
+      {
+        value: "non-jordanian",
+        label: t("reservation.options.nationality.non-jordanian"),
+      },
+    ],
+    [t],
+  );
+
+  {
+    /* submit */
+  }
+  function validate(): boolean {
+    const result = formSchema.safeParse(form);
+
+    if (!result.success) {
+      const fieldErrors: FormErrors = {};
+
+      result.error.issues.forEach((issue) => {
+        const field = issue.path[0] as keyof formType;
+        fieldErrors[field] = issue.message;
+      });
+
+      setFormErrors(fieldErrors);
+      return false;
+    }
+
+    setFormErrors({});
+    return true;
+  }
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (!validate()) {
+      console.log(formErrors);
+      return;
+    }
+
+    try {
+      // API call
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      console.log(form);
+      navigate("/organization-profile");
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div
@@ -54,7 +149,7 @@ export default function SignupForm({
       {/* FORM */}
       <Card className="rounded-none shadow-none dark:bg-slate-900">
         <CardContent className="p-6">
-          <form>
+          <form onSubmit={handleSubmit}>
             <FieldGroup>
               {/*  ID */}
               <Field>
@@ -64,6 +159,14 @@ export default function SignupForm({
                 </FieldLabel>
                 <Input
                   id="orgNationalId"
+                  type="number"
+                  value={form.orgNationalId}
+                  onChange={(e) =>
+                    setForm((prev) => ({
+                      ...prev,
+                      orgNationalId: Number(e.target.value),
+                    }))
+                  }
                   placeholder={t("auth.orgNationalIdPlaceholder")}
                   required
                 />
@@ -71,19 +174,26 @@ export default function SignupForm({
 
               {/*  name */}
               <Field>
-                <FieldLabel htmlFor="orgNationalName">
+                <FieldLabel htmlFor="orgName">
                   {t("auth.organizationName")}
                   <span className="text-red-500">*</span>
                 </FieldLabel>
                 <Input
-                  id="orgNationalName"
+                  id="orgName"
+                  value={form.orgName}
+                  onChange={(e) =>
+                    setForm((prev) => ({
+                      ...prev,
+                      orgName: e.target.value,
+                    }))
+                  }
                   placeholder={t("auth.organizationNamePlaceholder")}
                   required
                 />
               </Field>
 
               {/* company sector */}
-              <RadioGroup
+              {/* <RadioGroup
                 defaultValue="goverment"
                 onValueChange={(value) =>
                   setCompanySector(value as companySectorType)
@@ -98,19 +208,114 @@ export default function SignupForm({
                   <RadioGroupItem value="private" id="private" />
                   <Label htmlFor="private">{t("auth.private")}</Label>
                 </div>
-              </RadioGroup>
+              </RadioGroup> */}
 
+              {/* commissioner */}
+              <FieldGroup className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                {/* capacity */}
+                <Field>
+                  <FieldLabel htmlFor="capacity">
+                    {t("auth.capacity")} <span className="text-red-500">*</span>
+                  </FieldLabel>
+                  <Select
+                    value={form.capacity}
+                    onValueChange={(value) =>
+                      setForm((prev) => ({
+                        ...prev,
+                        capacity: value as formType["capacity"],
+                      }))
+                    }
+                    dir={t("dir")}
+                    required
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue
+                        placeholder={t("reservation.placeholders.select")}
+                      />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        {capacityOptions.map((option) => (
+                          <SelectItem key={option.value} value={option.value}>
+                            {option.label}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </Field>
+
+                {/* nationality */}
+                <Field>
+                  <FieldLabel htmlFor="nationality">
+                    {t("auth.nationality")}{" "}
+                    <span className="text-red-500">*</span>
+                  </FieldLabel>
+                  <Select
+                    value={form.nationality}
+                    onValueChange={(value) =>
+                      setForm((prev) => ({
+                        ...prev,
+                        nationality: value as formType["nationality"],
+                      }))
+                    }
+                    dir={t("dir")}
+                    required
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue
+                        placeholder={t("reservation.placeholders.select")}
+                      />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        {nationalityOptions.map((option) => (
+                          <SelectItem key={option.value} value={option.value}>
+                            {option.label}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </Field>
+              </FieldGroup>
+
+              {/* commissioner ID */}
+              <Field>
+                <FieldLabel htmlFor="commissionerID">
+                  {t("auth.commissionerID")}{" "}
+                  <span className="text-red-500">*</span>
+                </FieldLabel>
+                <Input
+                  id="commissionerID"
+                  type="number"
+                  value={form.commissionerID}
+                  onChange={(e) =>
+                    setForm((prev) => ({
+                      ...prev,
+                      commissionerID: Number(e.target.value),
+                    }))
+                  }
+                  placeholder={t("auth.commissionerIDPlaceholder")}
+                  required
+                />
+              </Field>
+
+              {/* email and pass */}
               <FieldGroup className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 {/* email */}
                 <Field>
                   <FieldLabel htmlFor="email">
-                    {t("profile.individual.email")}
+                    {t("auth.email")}
+                    <span className="text-red-500">*</span>
                   </FieldLabel>
                   <Input
                     id="email"
                     type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    value={form.email}
+                    onChange={(e) =>
+                      setForm((prev) => ({ ...prev, email: e.target.value }))
+                    }
                     placeholder={t("auth.emailPlaceholder")}
                     required
                   />
@@ -119,13 +324,16 @@ export default function SignupForm({
                 {/* phone number */}
                 <Field>
                   <FieldLabel htmlFor="phone">
-                    {t("profile.individual.phone")}
+                    {t("auth.phoneNumber")}{" "}
+                    <span className="text-red-500">*</span>
                   </FieldLabel>
                   <Input
                     id="phone"
                     type="tel"
-                    value={Phone}
-                    onChange={(e) => setPhone(e.target.value)}
+                    value={form.phone}
+                    onChange={(e) =>
+                      setForm((prev) => ({ ...prev, phone: e.target.value }))
+                    }
                     placeholder={t("auth.phoneNumberPlaceholder")}
                     required
                   />
@@ -161,9 +369,43 @@ export default function SignupForm({
                 </Field>
               </FieldGroup>
 
+              <Field>
+                <label className="block space-y-2">
+                  <span className="text-sm font-medium block">
+                    {t("auth.file")}
+                  </span>
+
+                  <span className="flex gap-2">
+                    <Button type="button" asChild>
+                      <label>
+                        {t("profile.chooseFile")}
+                        <input
+                          type="file"
+                          className="hidden"
+                          onChange={(e) =>
+                            setForm((prev) => ({
+                              ...prev,
+                              commissionerFile: e.target.files
+                                ? e.target.files[0]
+                                : undefined,
+                            }))
+                          }
+                        />
+                      </label>
+                    </Button>
+
+                    {form.commissionerFile && (
+                      <p className="text-sm text-muted-foreground">
+                        {form.commissionerFile.name}
+                      </p>
+                    )}
+                  </span>
+                </label>
+              </Field>
+
               {/* SUBMIT */}
               <Field>
-                <Button className="w-full" onClick={handleSubmit}>
+                <Button className="w-full" type="submit">
                   {t("auth.signup")}
                 </Button>
 
